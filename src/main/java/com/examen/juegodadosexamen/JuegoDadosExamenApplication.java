75790079286266ff316d24936a3ba77907780264
Juanjo <juanjetesc@gmail.com>
2025-02-08 12:11:42 +0100

    Initial commit

---
+++ src/main/java/com/examen/juegodadosexamen/JuegoDadosExamenApplication.java
@@ -0,0 +1,13 @@
+package com.examen.juegodadosexamen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuegoDadosExamenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JuegoDadosExamenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/examen/juegodadosexamen/JuegoDadosExamenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examen.juegodadosexamen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JuegoDadosExamenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
